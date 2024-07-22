fab2a90927236605a1c46364ca8ac788485afbc6
Matheus Santiago <matheus.nilo.santiago@gmail.com>
2024-07-22 13:51:38 -0300

    .

---
+++ copybook.py
@@ -0,0 +1,5 @@
+from tree_sitter import Language, Parser
+import tree_sitter_copybook
+
+COPYBOOK_LANGUAGE = Language(tspython.language())
+
--- main.py
+++ main.py
@@ -1,74 +1,46 @@
-from array import array
-import re
-import struct
-import decimal
-from parser import CopybookParser
+import tree_sitter_copybook as copybook
+from tree_sitter import Language, Parser
+
+COPYBOOK_LANGUAGE = Language(copybook.language())
+parser = Parser(COPYBOOK_LANGUAGE)
 
 
-# VER: https://gist.github.com/zorchenhimer/fd4d4208312d4175d106
-def unpack_comp_3(p):
-    a = array("B", p)
-    v = float(0)
-
-    # For all but last digit (half byte)
-    for i in a[:-1]:
-        v = (v * 100) + (((i & 0xF0) >> 4) * 10) + (i & 0xF)
-
-    # Last digit
-    i = a[-1]
-    v = (v * 10) + ((i & 0xF0) >> 4)
-
-    # Negative/Positve check.
-    if (i & 0xF) == 0xD:
-        v = -v
-
-    # Decimal points are determined by a COBOL program's PICtrue clauses, not
-    # the data on disk.
-    return int(v)
 
 
-def get_size_of_comp(n):
-    if n in range(1, 5):  # [1, 4]
-        return 2
-    if n in range(5, 10):  # [5, 9]
-        return 4
-    if n in range(10, 19):  # [10, 18]
-        return 8
-
-
-def unpack_comp(p):
-    return struct.unpack(">i", p)[0]
-
-
-if __name__ == "__main__":
-    parser = CopybookParser("copy.cpy")
-
-    descriptions = parser.get_data_description()
-
-    for desc in descriptions[:3]:
-        _type = desc["type"]
-        comp = desc["comp"]
-
-        digits = re.search(r"9+\(\d+\)", _type)
-        if not digits:
-            continue
-        digits = int(digits.group(0))
-
-        if comp == "COMP-3":
-            size = (digits // 2) + 1
-        if comp == "COMP":
-            size = get_size_of_comp(digits)
-        if comp is None:
-            size = digits
-
-
-        # print(f"name{desc['name']}: {size}")
-
-    # f = open("UT-S-ACH601S", "rb")
-    # f.seek(10)
-    # comp = f.read(4)
-
-    # convert hex to decimal
-    # print(struct.unpack(">i", comp)[0])
-
-    # f.close()
+# from pprint import pprint
+# import _copybook as copybook
+# from ebcdic import unpack
+#
+# def extract(group):
+#     curr_byte = 0
+#     children = []
+#     for k, v in group.items():
+#         if type(v) != dict:
+#             continue
+#
+#         name = k
+#         offset = v["bytes"]
+#
+#         _bytes = line[curr_byte : curr_byte + offset]
+#         content = unpack(_bytes, v["type"], True)
+#
+#         curr_byte += offset
+#
+#         if "FILLER" not in name:
+#             children.append((name, content))
+#     return children
+#
+# if __name__ == "__main__":
+#     with open("./sample-data/copybooks/DEBK1122.cpy") as f:
+#         cpy = copybook.toDict(f.readlines())
+#
+#     with open("./sample-data/files/BRT.DEB.DEB1122.D240118.D310.SS000110", "rb") as f:
+#
+#         line = f.read(13)
+#         headers = extract(cpy["DEB1122-REG-HEADER"])
+#
+#
+#         for i in range(15):
+#             line = f.read(13)
+#             det = extract(cpy["DEB1122-REG-DETALHE"])
+#             pprint(det)
